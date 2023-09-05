082ee01352bcc81ebf9943f2997fc8cf79b1fbc3
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-09-05 21:02:51 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js or webpack.config.js
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  // ...
+
+  images: {
+    domains: ['openweathermap.org'], // Add the domain(s) you want to allow for images
+  },
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          publicPath: '/_next/static/videos/', // Customize as per your project structure
+          outputPath: 'static/videos/', // Customize as per your project structure
+        },
+      },
+    });
+    
+
+    return config;
+  },
+};
