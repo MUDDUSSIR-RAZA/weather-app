2f9e134ac98048b03c084aef8999ca6c8397da2a
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-09-05 21:03:11 +0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import Bg from "@/components/Bg";
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>
+          Weather App
+        </title>
+      </Head>
+      <Bg>
+      <Component {...pageProps} />
+      </Bg>
+    </>
+  );
 }
